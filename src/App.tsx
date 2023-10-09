0a92c1c5ba00e0eb1b599f54ff729acf0f080f89
Cabral <Cabralzinho@users.noreply.github.com>
2023-10-09 13:27:55 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <WeatherProvider>
-        <div className='flex justify-center'>
+        <div className='flex justify-center items-center h-screen'>
           <div
             className='flex flex-col Sbg-gradient-to-tl bg-blue-500 dark:from-blue-900 dark:to-blue-950 mobile:w-[75%] p-4 mt-2 mobile:text-center rounded-2xl w-[50%] gap-2 container'>
             <Navbar />
